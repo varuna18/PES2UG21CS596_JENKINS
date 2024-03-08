f69212254b6f629ecff263e20c7d51d1a65e4bb6
varuna18 <varuna.simba@gmail.com>
2024-03-08 16:50:41 +0530

    Create working.cpp

---
+++ working.cpp
@@ -0,0 +1,11 @@
+//File: working.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "PIPELINE SUCCESSFUL\n";
+  return 0;
+}
